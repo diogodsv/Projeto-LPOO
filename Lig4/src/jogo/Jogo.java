3ddef5731df658b0da8aa6e56725d4a0fe40c9b8
Lyssa <lyssa.rodrigues@upe.br>
2023-07-03 20:51:16 -0300

    implementando o package jogo

--- Lig4/src/jogo/Jogo.java
+++ Lig4/src/jogo/Jogo.java
@@ -1,54 +1,56 @@
-package jogador;
+package jogo;
+
 import java.util.Scanner;
 
+import jogador.Cor;
+import jogador.Jogador;
 import tabuleiro.Tabuleiro;
 import tabuleiro.TabuleiroTurbo;
 
 public class Jogo {
 
-	private Tabuleiro tabuleiro;
+    private Tabuleiro tabuleiro;
     private Jogador jogador1;
     private Jogador jogador2;
     private Jogador jogadorAtual;
-	
+
     public void iniciarJogo() {
-    	Scanner sc = new Scanner(System.in);
-    	
-    	int modoDeJogo = 0;
-    	while(modoDeJogo != 1 || modoDeJogo != 2) {
-    		System.out.println("Escolha o modo de jogo:");
-    		System.out.println("[1] CLÁSSICO");
-    		System.out.println("[2] TURBO");
-    		modoDeJogo = sc.nextInt();
-    		sc.nextLine();
-    		if(modoDeJogo < 1 || modoDeJogo > 2) {
-    			System.out.println("Modo de jogo inválido.");
-    		} else if(modoDeJogo == 1) {
-    			tabuleiro = new Tabuleiro();
-    			break;
-    		} else if(modoDeJogo == 2) {
-    			tabuleiro = new TabuleiroTurbo();
-    			break;
-    		}
-    	}
-    	
-    	System.out.println("Nome do Jogador 1:");
-    	String nome1 = sc.nextLine();
-    	System.out.println("Nome do Jogador 2:");
-    	String nome2 = sc.nextLine();
-    	
-    	jogador1 = new Jogador(nome1, Cor.AMARELO);
+        Scanner sc = new Scanner(System.in);
+
+        int modoDeJogo = 0;
+        while (modoDeJogo != 1 || modoDeJogo != 2) {
+            System.out.println("Escolha o modo de jogo:");
+            System.out.println("[1] CLÁSSICO");
+            System.out.println("[2] TURBO");
+            modoDeJogo = sc.nextInt();
+            sc.nextLine();
+            if (modoDeJogo < 1 || modoDeJogo > 2) {
+                System.out.println("Modo de jogo inválido.");
+            } else if (modoDeJogo == 1) {
+                tabuleiro = new Tabuleiro();
+                break;
+            } else if (modoDeJogo == 2) {
+                tabuleiro = new TabuleiroTurbo();
+                break;
+            }
+        }
+
+        System.out.println("Nome do Jogador 1:");
+        String nome1 = sc.nextLine();
+        System.out.println("Nome do Jogador 2:");
+        String nome2 = sc.nextLine();
+
+        jogador1 = new Jogador(nome1, Cor.AMARELO);
         jogador2 = new Jogador(nome2, Cor.VERMELHO);
         jogadorAtual = jogador1;
-    	
-    	tabuleiro.inicializarTabuleiro();
+
+        tabuleiro.inicializarTabuleiro();
         tabuleiro.imprimirTabuleiro();
         int vitoria = 0;
 
         while (vitoria == 0 && !tabuleiro.isTabuleiroCheio()) {
             System.out.println(jogadorAtual.getNome() + ", digite a coluna (1-7) para inserir a peça: ");
-            int coluna = sc.nextInt()-1;
-        
+            int coluna = sc.nextInt() - 1;
 
             if (coluna < 0 || coluna >= Tabuleiro.COLUNAS) {
                 System.out.println("Coluna inválida. Escolha outra coluna.");
@@ -62,9 +64,9 @@ public class Jogo {
 
             tabuleiro.inserirPeca(coluna, jogadorAtual);
             tabuleiro.imprimirTabuleiro();
-            
+
             vitoria = tabuleiro.verificarVitoria();
-            
+
             if (vitoria == 0) {
                 jogadorAtual = (jogadorAtual == jogador1) ? jogador2 : jogador1;
             }
@@ -73,11 +75,11 @@ public class Jogo {
         if (vitoria == 1) {
             System.out.println("Parabéns, " + jogador1.getNome() + "! Você venceu o jogo!");
         } else if (vitoria == 2) {
-        	System.out.println("Parabéns, " + jogador2.getNome() + "! Você venceu o jogo!");
+            System.out.println("Parabéns, " + jogador2.getNome() + "! Você venceu o jogo!");
         } else {
-        	System.out.println("O jogo terminou em empate.");
+            System.out.println("O jogo terminou em empate.");
         }
         sc.close();
     }
-    
+
 }
--- Lig4/src/jogo/Main.java
+++ Lig4/src/jogo/Main.java
@@ -1,7 +1,7 @@
-package jogador;
+package jogo;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		Jogo game = new Jogo();
 		game.iniciarJogo();
