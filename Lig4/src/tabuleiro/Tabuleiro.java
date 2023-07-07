e58d8d0c23000a03eeb96be13f27a6e53a499a2f
Lyssa <lyssa.rodrigues@upe.br>
2023-07-06 23:52:18 -0300

    implementando a verificacao de vitoria no modo turbo

--- Lig4/src/tabuleiro/Tabuleiro.java
+++ Lig4/src/tabuleiro/Tabuleiro.java
@@ -5,19 +5,19 @@ import jogador.Jogador;
 import jogador.Peca;
 
 public class Tabuleiro {
-    private final int COLUNAS = 7;
-    private final int LINHAS = 6;
-    private Peca[][] grid;
-    private Peca peca1;
-    private Peca peca2;
+    protected static final int COLUNAS = 7;
+    protected static final int LINHAS = 6;
+    protected Peca[][] grid;
+    protected Peca peca1;
+    protected Peca peca2;
 
     public Tabuleiro() {
         grid = new Peca[LINHAS][COLUNAS];
     }
 
     public void inicializarTabuleiro() {
-        for (int i = 0; i < getLinhas(); i++) {
-            for (int j = 0; j < getColunas(); j++) {
+        for (int i = 0; i < LINHAS; i++) {
+            for (int j = 0; j < COLUNAS; j++) {
                 grid[i][j] = null;
             }
         }
@@ -34,27 +34,27 @@ public class Tabuleiro {
         }
 
         if (linha >= 0) {
-            Peca novaPeca = new Peca(jogador);
-            grid[linha][coluna] = novaPeca;
             if (jogador.getCor() == Cor.AMARELO) {
-                peca1 = novaPeca;
+                grid[linha][coluna] = new Peca(jogador);
+                peca1 = grid[linha][coluna];
             } else {
-                peca2 = novaPeca;
+                grid[linha][coluna] = new Peca(jogador);
+                peca2 = grid[linha][coluna];
             }
+
         }
     }
 
     public boolean verificarVitoriaVertical(Peca peca, String codigo) {
-        if (peca == null) {
-            return false;
-        }
         for (int i = 0; i < LINHAS - 3; i++) {
             for (int j = 0; j < COLUNAS; j++) {
-                if (grid[i][j] != null && grid[i][j] == peca &&
-                        grid[i + 1][j] == peca &&
-                        grid[i + 2][j] == peca &&
-                        grid[i + 3][j] == peca) {
-                    return true;
+                if (peca != null && grid[i][j] != null) {
+                    if (grid[i][j].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i + 1][j].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i + 2][j].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i + 3][j].getJogador().getCor().getCodigo().equals(codigo)) {
+                        return true;
+                    }
                 }
             }
         }
@@ -62,16 +62,16 @@ public class Tabuleiro {
     }
 
     public boolean verificarVitoriaHorizontal(Peca peca, String codigo) {
-        if (peca == null) {
-            return false;
-        }
         for (int i = 0; i < LINHAS; i++) {
             for (int j = 0; j < COLUNAS - 3; j++) {
-                if (grid[i][j] != null && grid[i][j] == peca &&
-                        grid[i][j + 1] == peca &&
-                        grid[i][j + 2] == peca &&
-                        grid[i][j + 3] == peca) {
-                    return true;
+                if (peca != null && grid[i][j] != null &&
+                        (grid[i][j + 1] != null && grid[i][j + 2] != null && grid[i][j + 3] != null)) {
+                    if ((grid[i][j].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i][j + 1].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i][j + 2].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i][j + 3].getJogador().getCor().getCodigo().equals(codigo))) {
+                        return true;
+                    }
                 }
             }
         }
@@ -81,11 +81,16 @@ public class Tabuleiro {
     public boolean verificarVitoriaDiagonalAscendente(Peca peca, String codigo) {
         for (int i = 3; i < LINHAS; i++) {
             for (int j = 0; j < COLUNAS - 3; j++) {
-                if (grid[i][j] != null && grid[i][j] == peca &&
-                        grid[i - 1][j + 1] == peca &&
-                        grid[i - 2][j + 2] == peca &&
-                        grid[i - 3][j + 3] == peca) {
-                    return true;
+                if (peca != null && grid[i][j] != null &&
+                        grid[i - 1][j + 1] != null &&
+                        grid[i - 2][j + 2] != null &&
+                        grid[i - 3][j + 3] != null) {
+                    if (grid[i][j].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i - 1][j + 1].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i - 2][j + 2].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i - 3][j + 3].getJogador().getCor().getCodigo().equals(codigo)) {
+                        return true;
+                    }
                 }
             }
         }
@@ -95,11 +100,16 @@ public class Tabuleiro {
     public boolean verificarVitoriaDiagonalDescendente(Peca peca, String codigo) {
         for (int i = 0; i < LINHAS - 3; i++) {
             for (int j = 0; j < COLUNAS - 3; j++) {
-                if (grid[i][j] != null && grid[i][j] == peca &&
-                        grid[i + 1][j + 1] == peca &&
-                        grid[i + 2][j + 2] == peca &&
-                        grid[i + 3][j + 3] == peca) {
-                    return true;
+                if (peca != null && grid[i][j] != null &&
+                        grid[i + 1][j + 1] != null &&
+                        grid[i + 2][j + 2] != null &&
+                        grid[i + 3][j + 3] != null) {
+                    if (grid[i][j].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i + 1][j + 1].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i + 2][j + 2].getJogador().getCor().getCodigo().equals(codigo) &&
+                            grid[i + 3][j + 3].getJogador().getCor().getCodigo().equals(codigo)) {
+                        return true;
+                    }
                 }
             }
         }
@@ -147,14 +157,6 @@ public class Tabuleiro {
         System.out.println("1 2 3 4 5 6 7");
     }
 
-    public int getColunas() {
-        return COLUNAS;
-    }
-
-    public int getLinhas() {
-        return LINHAS;
-    }
-
     public Peca[][] getGrid() {
         return grid;
     }
@@ -166,4 +168,13 @@ public class Tabuleiro {
     public Peca getPeca2() {
         return peca2;
     }
+
+    public int getColunas() {
+        return COLUNAS;
+    }
+
+    public int getLinhas() {
+        return LINHAS;
+    }
+
 }
--- Lig4/src/tabuleiro/TabuleiroTurbo.java
+++ Lig4/src/tabuleiro/TabuleiroTurbo.java
@@ -8,6 +8,7 @@ public class TabuleiroTurbo extends Tabuleiro {
 
     public void inserirPeca(int coluna, Jogador jogador) {
         Peca[][] grid = getGrid();
+
         int linha = getLinhas() - 1;
         while (linha >= 0 && grid[linha][coluna] != null) {
             linha--;
@@ -16,25 +17,51 @@ public class TabuleiroTurbo extends Tabuleiro {
         if (linha >= 0) {
             if (jogador.getCor() == Cor.AMARELO) {
                 grid[linha][coluna] = new Peca(jogador);
+                peca1 = grid[linha][coluna];
             } else {
                 grid[linha][coluna] = new Peca(jogador);
+                peca2 = grid[linha][coluna];
             }
 
             alterarPecasVizinhas(linha, coluna, jogador);
         }
     }
 
-    public void alterarPecasVizinhas(int linha, int coluna, Jogador jogador) {
+    private void alterarPecasVizinhas(int linha, int coluna, Jogador jogador) {
         alterarPecaVizinha(linha, coluna - 1, jogador);
         alterarPecaVizinha(linha, coluna + 1, jogador);
     }
 
-    public void alterarPecaVizinha(int linha, int coluna, Jogador jogador) {
+    private void alterarPecaVizinha(int linha, int coluna, Jogador jogador) {
         Peca[][] grid = getGrid();
-        if (linha >= 0 && linha < getLinhas() && coluna >= 0 && coluna < getColunas() && grid[linha][coluna] != null) {
+        int linhas = getLinhas();
+        int colunas = getColunas();
+
+        if (linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas && grid[linha][coluna] != null) {
             Peca pecaVizinha = new Peca(jogador);
             pecaVizinha.setCor(jogador.getCor());
             grid[linha][coluna] = pecaVizinha;
         }
     }
+
+    @Override
+    public int verificarVitoria() {
+        Peca peca1 = getPeca1();
+        Peca peca2 = getPeca2();
+
+        if (peca1 != null && (verificarVitoriaVertical(peca1, "A") ||
+                verificarVitoriaHorizontal(peca1, "A") ||
+                verificarVitoriaDiagonalAscendente(peca1, "A") ||
+                verificarVitoriaDiagonalDescendente(peca1, "A"))) {
+            return 1;
+        } else if (peca2 != null && (verificarVitoriaVertical(peca2, "V") ||
+                verificarVitoriaHorizontal(peca2, "V") ||
+                verificarVitoriaDiagonalAscendente(peca2, "V") ||
+                verificarVitoriaDiagonalDescendente(peca2, "V"))) {
+            return 2;
+        }
+
+        return 0;
+    }
+
 }
